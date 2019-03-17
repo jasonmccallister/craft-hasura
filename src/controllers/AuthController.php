131860c6a104c4ae47afdf356d03efeeb783496d
Jason McCallister <themccallister@gmail.com>
2019-03-17 17:15:49 -0400

    add logo and update the authcontroller

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -11,9 +11,8 @@
 namespace jasonmccallister\hasura\controllers;
 
 use Craft;
-use Firebase\JWT\JWT;
 use craft\web\Controller;
-use jasonmccallister\hasura\Hasura;
+use jasonmccallister\hasura\Encoder;
 
 /**
  * Auth Controller
@@ -52,6 +51,19 @@ class AuthController extends Controller
     // =========================================================================
 
     /**
+     * @inheritdoc
+     */
+    public function beforeAction($action)
+    {
+        // Don't enable CSRF validation for auth requests
+        if ($action->id === 'index') {
+            $this->enableCsrfValidation = false;
+        }
+
+        return parent::beforeAction($action);
+    }
+
+    /**
      * Handle a request going to our plugin's index action URL,
      * e.g.: actions/hasura/auth
      *
@@ -59,62 +71,20 @@ class AuthController extends Controller
      */
     public function actionIndex()
     {
-        // TODO disable CSRF for this request
-
-        // make sure its post
-        if (!Craft::$app->getRequest()->getIsPost()) {
-            // TODO make this return a useful JSON response
-            return $this->asErrorJson('You must send a POST request');
-        }
+        $this->requirePostRequest();
 
         $loginName = Craft::$app->getRequest()->getRequiredBodyParam('loginName');
         $password = Craft::$app->getRequest()->getRequiredBodyParam('password');
-
         $user = Craft::$app->getUsers()->getUserByUsernameOrEmail($loginName);
 
         if (!$user->authenticate($password)) {
-            // TODO make this return a useful JSON response
             return $this->asErrorJson('Unable to authenticate the user');
         }
 
-        if (!Craft::$app->getUser()->login($user, 1)) {
-            // TODO make this return a useful JSON response
-            return $this->asErrorJson('Unable to authenticate the user');
-        }
+        $generalConfig = Craft::$app->getConfig()->getGeneral();
 
-        // TODO remove this example code from JWT into a config or env var
-        $privateKey = <<<EOD
------BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQC8kGa1pSjbSYZVebtTRBLxBz5H4i2p/llLCrEeQhta5kaQu/Rn
-vuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t0tyazyZ8JXw+KgXTxldMPEL9
-5+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4ehde/zUxo6UvS7UrBQIDAQAB
-AoGAb/MXV46XxCFRxNuB8LyAtmLDgi/xRnTAlMHjSACddwkyKem8//8eZtw9fzxz
-bWZ/1/doQOuHBGYZU8aDzzj59FZ78dyzNFoF91hbvZKkg+6wGyd/LrGVEB+Xre0J
-Nil0GReM2AHDNZUYRv+HYJPIOrB0CRczLQsgFJ8K6aAD6F0CQQDzbpjYdx10qgK1
-cP59UHiHjPZYC0loEsk7s+hUmT3QHerAQJMZWC11Qrn2N+ybwwNblDKv+s5qgMQ5
-5tNoQ9IfAkEAxkyffU6ythpg/H0Ixe1I2rd0GbF05biIzO/i77Det3n4YsJVlDck
-ZkcvY3SK2iRIL4c9yY6hlIhs+K9wXTtGWwJBAO9Dskl48mO7woPR9uD22jDpNSwe
-k90OMepTjzSvlhjbfuPN1IdhqvSJTDychRwn1kIJ7LQZgQ8fVz9OCFZ/6qMCQGOb
-qaGwHmUK6xzpUbbacnYrIM6nLSkXgOAwv7XXCojvY614ILTK3iXiLBOxPu5Eu13k
-eUz9sHyD6vkgZzjtxXECQAkp4Xerf5TGfQXGXhxIX52yH+N2LtujCdkQZjXAsGdm
-B2zNzvrlgRmgBrklMTrMYgm1NPcW+bRLGcwgW2PTvNM=
------END RSA PRIVATE KEY-----
-EOD;
+        $token = Encoder::encode($user, $generalConfig->userSessionDuration);
 
-        $token = [
-            'sub' => $user->uid,
-            'admin' => $user->admin ?? false,
-            'iat' => time(),
-            'https://hasura.io/jwt/claims' => [ // TODO make this a configuration from hasura config `claims_namespace`
-                'x-hasura-allowed-roles' => ['editor', 'user', 'mod'], // TODO make this grab the kabob version of the users groups
-                'x-hasura-default-role' => 'user', // TODO grab the users first group? Maybe make this a configuration option to select the default group?
-                'x-hasura-user-id' => $user->uid,
-                'x-hasura-org-id' => '123' // TODO support additional claims, possibly by grabbing the attributes on the users fields?
-            ]
-        ];
-
-        $encoded = JWT::encode($token, $privateKey, 'RS256');
-
-        return $this->asJson(['token' => $encoded]);
+        return $this->asJson(['token' => $token]);
     }
 }
