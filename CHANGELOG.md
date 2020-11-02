0b38acde9e24498c90096da35f87c7fed73c2351
Jason McCallister <jason@craftcms.com>
2020-11-02 10:56:13 -0500

    use webhook header setting
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,16 @@
 # Hasura Changelog
 
+### Fixed
+- An issue where the `webhookHeader` default setting was not used.
+
 ## 1.2.0 - 2020-10-26
 
 ### Added
-
 - Add ability to write custom claims via twig
 
 ### Fixed
 
-- Fixed an issue with Composer 2.0 compatability. [#11](https://github.com/jasonmccallister/craft-hasura/issues/11)
+- Fixed an issue with Composer 2.0 compatibility. [#11](https://github.com/jasonmccallister/craft-hasura/issues/11)
 
 ## 1.1.0 - 2019-05-24
 
--- src/controllers/WebhookController.php
+++ src/controllers/WebhookController.php
@@ -6,6 +6,8 @@ use Craft;
 use craft\web\Controller;
 use jasonmccallister\hasura\events\HasuraEvent;
 use jasonmccallister\hasura\Hasura;
+use jasonmccallister\hasura\models\Settings;
+use yii\web\BadRequestHttpException;
 
 class WebhookController extends Controller
 {
@@ -34,25 +36,33 @@ class WebhookController extends Controller
      * e.g.: actions/hasura/webhook
      *
      * @return mixed
+     * @throws BadRequestHttpException
      */
     public function actionIndex()
     {
         $this->requirePostRequest();
         $request = Craft::$app->getRequest();
+        /** @var Settings $settings */
         $settings = Hasura::$plugin->getSettings();
 
-        if ($request->getHeaders()->get('x-api-key') !== $settings->webhookKey) {
+        if ($request->getHeaders()->get($settings->webhookHeader) !== $settings->webhookKey) {
             Craft::$app->getResponse()->setStatusCode(400);
+
             return $this->asErrorJson('Unable to authenticate with the webhook');
         }
 
         $payload = json_decode($request->rawBody, true);
 
-        $this->trigger('hasuraEventTrigger', new HasuraEvent([
-            'trigger' => $payload['trigger']['name'],
-            'table' => $payload['table']['name'],
-            'payload' => $payload,
-        ]));
+        $this->trigger(
+            'hasuraEventTrigger',
+            new HasuraEvent(
+                [
+                    'trigger' => $payload['trigger']['name'],
+                    'table' => $payload['table']['name'],
+                    'payload' => $payload,
+                ]
+            )
+        );
 
         return $this->asJson(['success' => 'event trigger ' . $payload['id'] . ' received']);
     }
